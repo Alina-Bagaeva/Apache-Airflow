012d84df35548460c383c286d5e5acad29c62a0b
root <root@cv5619595.novalocal>
2025-12-23 14:17:18 +0300

    Mini production tuning on server

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+BACKUP_DIR="./backups"
+mkdir -p "$BACKUP_DIR"
+
+docker compose exec -T postgres \
+  pg_dump -U airflow airflow > "$BACKUP_DIR/airflow_backup_${TIMESTAMP}.sql"
+
+echo "Backup created: $BACKUP_DIR/airflow_backup_${TIMESTAMP}.sql"
