a62bfde338330e82ec0734ffdae4723f4ac416ce
Багаева Алина <egypt.myau@gmail.com>
2026-01-10 17:51:15 +0300

    update readme.md, backup.sh

--- backup.sh
+++ backup.sh
@@ -7,3 +7,6 @@ docker compose exec -T postgres \
   pg_dump -U airflow airflow > "$BACKUP_DIR/airflow_backup_${TIMESTAMP}.sql"
 
 echo "Backup created: $BACKUP_DIR/airflow_backup_${TIMESTAMP}.sql"
+
+find /root/Apache-Airflow/backups/ -name "*.sql" -mtime +30 -delete
+find /root/Apache-Airflow/backups/ -name "*.log" -mtime +30 -delete
